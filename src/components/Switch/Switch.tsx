a0fd507831c2c784d96b5eba69ecb030740878b5
Keshav Kishor Ram <ramabishek40@gmail.com>
2024-06-29 11:33:06 +0545

    feat: handle state inside the switch component

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useId } from "react";
+import { ChangeEvent, useEffect, useId, useState } from "react";
 import { SwitchContainer } from "./Switch.styled";
 
 export type SwitchProps = {
@@ -9,9 +9,25 @@ export type SwitchProps = {
 export default function Switch(props: SwitchProps) {
   const { isChecked = false, onChange } = props;
   const id = useId();
+  const [checked, setChecked] = useState(isChecked);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setChecked(e.target.checked);
+    onChange?.(e);
+  };
+
+  useEffect(() => {
+    setChecked(isChecked);
+  }, [isChecked]);
+
   return (
     <SwitchContainer>
-      <input type="checkbox" id={id} checked={isChecked} onChange={onChange} />
+      <input
+        type="checkbox"
+        id={id}
+        checked={checked}
+        onChange={handleChange}
+      />
       <label className="slider round" htmlFor={id}></label>
     </SwitchContainer>
   );
