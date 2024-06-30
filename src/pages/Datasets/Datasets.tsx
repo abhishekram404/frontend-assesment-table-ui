44ea9cc2e48a3a07b7cada8e00baa2176cbc76ba
Keshav Kishor Ram <ramabishek40@gmail.com>
2024-06-30 20:16:03 +0545

    perf: dynamically import the table component to improve performance

--- src/pages/Datasets/Datasets.tsx
+++ src/pages/Datasets/Datasets.tsx
@@ -1,15 +1,22 @@
-import Table from "../../components/Table/Table";
+import { lazy, Suspense } from "react";
+import LoadingScreen from "../../components/LoadingScreen/LoadingScreen";
 import useProducts from "../../hooks/useProducts";
 import Topbar from "./components/Topbar";
 import { Root } from "./Datasets.styled";
 import { columns } from "./Datasets.utils";
+const Table = lazy(() => import("../../components/Table/Table"));
 
 export default function Datasets() {
-  const { products } = useProducts();
+  const { products, isFetching } = useProducts();
+
+  if (isFetching) return <LoadingScreen />;
+
   return (
     <Root>
       <Topbar />
-      <Table columns={columns} items={products} />
+      <Suspense fallback={<LoadingScreen />}>
+        <Table columns={columns} items={products} />
+      </Suspense>
     </Root>
   );
 }
