fd18597a3768e0b322f3b3fdd24be35636b28064
Keshav Kishor Ram <ramabishek40@gmail.com>
2024-06-30 15:17:47 +0545

    refactor: extract out the data and fetching functionality into separate files and hooks

--- src/pages/Datasets/Datasets.tsx
+++ src/pages/Datasets/Datasets.tsx
@@ -1,45 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
-import Table, { TableColumn } from "../../components/Table/Table";
+import Table from "../../components/Table/Table";
+import useProducts from "../../hooks/useProducts";
 import Topbar from "./components/Topbar";
 import { Root } from "./Datasets.styled";
-import Pill from "../../components/Pill/Pill";
-
-const columns: TableColumn[] = [
-  { name: "Product name", accessor: "name" },
-  {
-    name: "Price",
-    accessor: "price",
-    prefix: "$",
-    formatter: (value) => parseInt(value as string),
-  },
-  { name: "Rating", accessor: "rating" },
-  { name: "Brand", accessor: "brand" },
-  {
-    name: "Availability",
-    accessor: "isAvailable",
-    formatter: (value) =>
-      value ? (
-        <Pill backgroundColor="#28a745" color="#ffffff">
-          Available
-        </Pill>
-      ) : (
-        <Pill backgroundColor="#dc3545" color="#ffffff">
-          Out of stock
-        </Pill>
-      ),
-  },
-];
+import { columns } from "./Datasets.utils";
 
 export default function Datasets() {
-  const { data: products = [] } = useQuery({
-    queryKey: ["products"],
-    queryFn: async () => {
-      const response = await fetch(
-        "https://667e8a33f2cb59c38dc61920.mockapi.io/products"
-      );
-      return response.json();
-    },
-  });
+  const { products } = useProducts();
   return (
     <Root>
       <Topbar />
