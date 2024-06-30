ee21ae673bcc6a3be8ea9d024465252b91de1484
Keshav Kishor Ram <ramabishek40@gmail.com>
2024-06-30 15:07:12 +0545

    feat: integrate the mockapi API for fetching products data

--- src/pages/Datasets/Datasets.tsx
+++ src/pages/Datasets/Datasets.tsx
@@ -1,28 +1,49 @@
-import Table from "../../components/Table/Table";
+import { useQuery } from "@tanstack/react-query";
+import Table, { TableColumn } from "../../components/Table/Table";
 import Topbar from "./components/Topbar";
 import { Root } from "./Datasets.styled";
+import Pill from "../../components/Pill/Pill";
 
-const columns = [
+const columns: TableColumn[] = [
   { name: "Product name", accessor: "name" },
-  { name: "Price", accessor: "price", prefix: "$" },
+  {
+    name: "Price",
+    accessor: "price",
+    prefix: "$",
+    formatter: (value) => parseInt(value as string),
+  },
   { name: "Rating", accessor: "rating" },
   { name: "Brand", accessor: "brand" },
-  { name: "Availability", accessor: "isAvailable" },
+  {
+    name: "Availability",
+    accessor: "isAvailable",
+    formatter: (value) =>
+      value ? (
+        <Pill backgroundColor="#28a745" color="#ffffff">
+          Available
+        </Pill>
+      ) : (
+        <Pill backgroundColor="#dc3545" color="#ffffff">
+          Out of stock
+        </Pill>
+      ),
+  },
 ];
 
-const items = Array(100).fill({
-  name: "Product 1",
-  price: 100,
-  rating: 4.5,
-  brand: "Brand 1",
-  isAvailable: "Available",
-});
-
 export default function Datasets() {
+  const { data: products = [] } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const response = await fetch(
+        "https://667e8a33f2cb59c38dc61920.mockapi.io/products"
+      );
+      return response.json();
+    },
+  });
   return (
     <Root>
       <Topbar />
-      <Table columns={columns} items={items} />
+      <Table columns={columns} items={products} />
     </Root>
   );
 }
